6c4fc325a56eb9173f686cefb9a647666c3a3133
Eunha Hwang <45348138+eeunha@users.noreply.github.com>
2023-10-23 16:59:15 +0900

    [level unrated] Title: 문자열의 뒤의 n글자, Time: 0.01 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.java
@@ -0,0 +1,10 @@
+/*
+    ProgrammerS123 -> 14개
+    grammerS123 -> 11개
+    my_string.substring(my_string.length() - n)
+*/
+class Solution {
+    public String solution(String my_string, int n) {
+        return my_string.substring(my_string.length() - n);
+    }
+}
