f1f2ebd343ca7f99d6faecc572d6f37bd8fab526
Eunha Hwang <45348138+eeunha@users.noreply.github.com>
2023-10-23 16:12:39 +0900

    [level unrated] Title: 세균 증식, Time: 0.02 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,13 @@
+//세균 증식
+/*
+
+*/
+class Solution {
+    public int solution(int n, int t) {
+        
+        for (int i = 1; i <= t; i++) {
+            n *= 2;
+        }
+        return n;
+    }
+}
