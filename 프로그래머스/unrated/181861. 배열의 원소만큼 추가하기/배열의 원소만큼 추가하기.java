1d2f99d95c6dedc7400a7e9bc8016c4e5f6d462b
Eunha Hwang <45348138+eeunha@users.noreply.github.com>
2023-10-24 16:22:34 +0900

    Update 배열의 원소만큼 추가하기.java

--- 프로그래머스/unrated/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.java
+++ 프로그래머스/unrated/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.java
@@ -9,4 +9,19 @@ class Solution {
         }
         return list.stream().mapToInt(i -> i).toArray();
     }
+
+    public int[] solution2(int[] arr) {
+        int sum = 0;
+        for (int n : arr) {
+            sum += n;
+        }
+        int[] answer = new int[sum];
+        int idx = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i]; j++) {
+                answer[idx++] = arr[i];
+            }
+        }
+        return answer;
+    }
 }
