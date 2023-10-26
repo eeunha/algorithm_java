cd5ab207a6d39867bb0d60f9dce0804a05155b06
Eunha Hwang <45348138+eeunha@users.noreply.github.com>
2023-10-26 10:06:39 +0900

    Update 대문자와 소문자.java

--- 프로그래머스/unrated/120893. 대문자와 소문자/대문자와 소문자.java
+++ 프로그래머스/unrated/120893. 대문자와 소문자/대문자와 소문자.java
@@ -15,4 +15,20 @@ class Solution {
         
         return sb.toString();
     }
+
+    public String solution2(String my_string) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < my_string.length(); i++) {
+            int c = my_string.charAt(i);
+            
+            if (c >= 97 && c <= 122) {
+                sb.append((char)(c - 32));
+            } else if (c >= 65 && c <= 90) {
+                sb.append((char)(c + 32));
+            }
+        }
+        
+        return sb.toString();
+    }
 }
