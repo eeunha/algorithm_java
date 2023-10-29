f5e9c4b42a585c93ae35a474fb1ba8b946638c3e
Eunha Hwang <45348138+eeunha@users.noreply.github.com>
2023-10-29 13:55:31 +0900

    Update 오프라인／온라인 판매 데이터 통합하기.sql

--- 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -1,10 +1,4 @@
 -- 코드를 입력하세요
--- select sales_date, online_sale_id as sale_id, product_id, user_id, sales_amount
--- from online_sale
--- union
--- select sales_date, offline_sale_id as sale_id, product_id, null as user_id, sales_amount
--- from offline_sale;
-
 select to_char(sales_date, 'yyyy-mm-dd') as sales_date, product_id, user_id, sales_amount
 from (select sales_date, product_id, user_id, sales_amount
 from online_sale
