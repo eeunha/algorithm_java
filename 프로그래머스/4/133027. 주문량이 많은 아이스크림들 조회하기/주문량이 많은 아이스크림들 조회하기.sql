596c902236508db49a374b74d86e2dc8866f0de9
Eunha Hwang <45348138+eeunha@users.noreply.github.com>
2023-10-30 09:46:38 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,11 +1,18 @@
-select flavor
-from (
-    select flavor, rownum
-    from (
-        select flavor
-        from (select * from first_half
-            union
-            select * from july)
-        group by flavor
-        order by sum(total_order) desc))
-where rownum <= 3;
+-- select flavor
+-- from (
+--     select flavor, rownum
+--     from (
+--         select flavor
+--         from (select * from first_half
+--             union
+--             select * from july)
+--         group by flavor
+--         order by sum(total_order) desc))
+-- where rownum <= 3;
+
+select h.flavor
+from first_half h, july j
+where h.flavor = j.flavor
+group by h.flavor
+order by sum(h.total_order + j.total_order) desc
+FETCH FIRST 3 ROWS ONLY;
