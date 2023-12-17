c6733ea10f7659c6b420952bb0fdc342dab271e1
Eunha Hwang <45348138+eeunha@users.noreply.github.com>
2023-12-17 13:02:31 +0900

    [level unrated] Title: 한 번만 등장한 문자, Time: 0.02 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        
+        //edge case
+        if (s.length() == 1) return s;
+        
+        StringBuilder sb = new StringBuilder();
+        
+        int[] countArr = new int[26];
+        
+        for (int i = 0; i < s.length(); i++) {
+            char curC = s.charAt(i);
+            
+            countArr[curC - 97]++;
+        }
+        
+        for (int i = 0; i < countArr.length; i++) {
+            if (countArr[i] == 1) {
+                sb.append((char)(i + 97));
+            }
+        }
+        
+        return sb.toString();
+    }
+}
