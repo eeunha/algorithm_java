9397ae38c5b85186612d031cd57abd6a6c38c227
Eunha Hwang <45348138+eeunha@users.noreply.github.com>
2023-12-29 09:05:27 +0900

    [level 2] Title: 프로세스, Time: 0.62 ms, Memory: 82.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] priorities, int location) {
+		int answer = 0; // 빠져나간 개수
+		Queue<Integer> queue = new LinkedList<Integer>();
+
+		int curMaxIdx = priorities.length - 1; // 가장 큰 우선순위가 있는 인덱스
+
+		for (int i = 0; i < priorities.length; i++) {
+			queue.add(priorities[i]);
+		}
+
+		Arrays.sort(priorities); // 우선순위를 정렬함
+
+		while (curMaxIdx >= 0) {
+			if (queue.peek() == priorities[curMaxIdx]) {
+				// 꺼내기
+				queue.poll();
+				answer++;
+
+				if (location == 0) {// 내가 찾는 값인 경우
+					break;
+				}
+
+				location--;
+				curMaxIdx--;
+
+			} else {
+				// 다음 수로 이동하기
+				int n = queue.poll();
+				queue.add(n);
+
+				if (location == 0) { // 내가 찾는 값인 경우
+					location = queue.size() - 1;
+				} else {
+					location--;
+				}
+			}
+		}
+
+		return answer;
+	}
+}
