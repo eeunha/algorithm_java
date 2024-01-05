4fe879c3ae389e19200140cae4a823e36bffa9d4
Eunha Hwang <45348138+eeunha@users.noreply.github.com>
2024-01-05 11:12:47 +0900

    [level unrated] Title: 문자열 밀기, Time: 0.03 ms, Memory: 68.9 MB -BaekjoonHub

--- 프로그래머스/unrated/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/unrated/120921. 문자열 밀기/문자열 밀기.java
@@ -4,20 +4,8 @@ class Solution {
         if (A.equals(B)) return 0;
         if (A.length() == 1) return -1;
         
-        StringBuilder sb = new StringBuilder();
+        String str = B.repeat(3);
         
-        int answer = -1;
-  
-        for (int i = A.length() - 1; i >= 0; i--) {
-            sb = new StringBuilder();
-            sb.append(A.substring(i));
-            sb.append(A.substring(0, i));
-            
-            if (sb.toString().equals(B)) {
-                return A.length() - i;
-            }
-        }
-        
-        return  answer;
+        return str.indexOf(A);
     }
 }
