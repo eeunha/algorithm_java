2b15b12cd65d719cde316141bacd92796805a9f3
Eunha Hwang <45348138+eeunha@users.noreply.github.com>
2024-01-05 23:18:45 +0900

    Update 연속된 수의 합.java

--- 프로그래머스/unrated/120923. 연속된 수의 합/연속된 수의 합.java
+++ 프로그래머스/unrated/120923. 연속된 수의 합/연속된 수의 합.java
@@ -2,20 +2,16 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int num, int total) {
-        ArrayList<Integer> list = new ArrayList<>();
+        int[] arr = new int[num];
         
-        int n = 0;
-        
-        for (int i = 1; num > 1 && i < num; i++) {
-            n += i;
-        }
+        int n = (num - 1) * num / 2;
         
         int startN = (int) ((total - n) / num);
         
         for (int i = 0; i < num; i++) {
-            list.add(startN + i);
+            arr[i] = startN + i;
         }
         
-        return list.stream().mapToInt(i -> i).toArray();
+        return arr;
     }
 }
