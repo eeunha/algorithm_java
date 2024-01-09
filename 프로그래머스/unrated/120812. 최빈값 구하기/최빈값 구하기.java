1b20b1595e42e55a5f46e46e7daa0db7533984ce
Eunha Hwang <45348138+eeunha@users.noreply.github.com>
2024-01-09 15:57:35 +0900

    [level unrated] Title: 최빈값 구하기, Time: 0.48 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120812. 최빈값 구하기/최빈값 구하기.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array) {
+        //edge cases
+        if (array.length == 1) return array[0];
+        
+        int answer = 0;
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int n : array) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+        
+        int mode = 0;
+        int modeCount = 0;
+        
+        for (int key : map.keySet()) {
+            if (modeCount < map.get(key)) {
+                mode = key;
+                modeCount = map.get(key);
+            }
+        }
+        
+        System.out.println(mode);
+        
+        for (int key : map.keySet()) {
+            if (modeCount == map.get(key) && key != mode) {
+                return -1;
+            }
+        }
+        
+        return mode;
+    }
+}
