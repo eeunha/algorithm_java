5dfed8b4ec2072d392ec8c8d9ae1ec9f7af965a3
Eunha Hwang <45348138+eeunha@users.noreply.github.com>
2024-01-13 21:29:41 +0900

    [level Lv.0] Title: 특별한 이차원 배열 2, Time: 0.22 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
+++ 프로그래머스/Lv.0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
@@ -1,6 +1,6 @@
 class Solution {
     public int solution(int[][] arr) {
-        for (int i = 0; i < arr.length / 2 + 1; i++) {
+        for (int i = 0; i < Math.ceil(arr.length / 2); i++) {
             for (int j = 0; j < arr[0].length; j++) {
                 if (arr[i][j] != arr[j][i]) {
                     return 0;
