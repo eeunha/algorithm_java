22d96966895b88e3d0bc46a3e3a1f4014d511b21
Eunha Hwang <45348138+eeunha@users.noreply.github.com>
2024-01-18 09:48:05 +0900

    [level 0] Title: 세로 읽기, Time: 0.03 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -0,0 +1,32 @@
+/*
+i % m == 0 -> 새로운 줄의 시작
+
+m=4, c=2
+ihrh -> h -> 1 => (2-1) + m * 0 = 1 + 4 * 0 = 1
+bakr -> a -> 5 => 1 + 4 * 1 = 5
+fpnd -> p -> 9
+oplj -> 
+hygc
+
+for (int i = 0; i < (my_string.length() - (c - 1)) / m; i++)
+(c-1) + m * i
+
+int curIdx = c - 1;
+int i = 0;
+while (curIdx < my_string.length()) {
+    sb.append(my_string.charAt(curIdx));
+    i++;
+    curIdx += m * i;
+}
+*/
+class Solution {
+    public String solution(String my_string, int m, int c) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i * m < my_string.length() - (c - 1); i++) {
+            sb.append(my_string.charAt((c - 1) + m * i));
+        }
+        
+        return sb.toString();
+    }
+}
