69ec628517c04cdd4c5505630c20bb59119f286a
Eunha Hwang <45348138+eeunha@users.noreply.github.com>
2024-01-20 19:30:12 +0900

    [level 0] Title: 모스부호 (1), Time: 0.14 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String letter) {
+        StringBuilder sb = new StringBuilder();
+        
+        HashMap<String, String> map = new HashMap<>();
+        map.put(".-", "a");
+        map.put("-...", "b");
+        map.put("-.-.", "c");
+        map.put("-..", "d");
+        map.put(".", "e");
+        map.put("..-.", "f");
+        map.put("--.", "g");
+        map.put("....", "h");
+        map.put("..", "i");
+        map.put(".---", "j");
+        map.put("-.-", "k");
+        map.put(".-..", "l");
+        map.put("--", "m");
+        map.put("-.", "n");
+        map.put("---", "o");
+        map.put(".--.", "p");
+        map.put("--.-", "q");
+        map.put(".-.", "r");
+        map.put("...", "s");
+        map.put("-", "t");
+        map.put("..-", "u");
+        map.put("...-", "v");
+        map.put(".--", "w");
+        map.put("-..-", "x");
+        map.put("-.--", "y");
+        map.put("--..", "z");
+        
+        String[] arr = letter.split(" ");
+        
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(map.get(arr[i]));
+        }
+        
+        return sb.toString();
+    }
+}
