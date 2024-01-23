305076042cc91fa7023964ae33ca2c111120ee01
Eunha Hwang <45348138+eeunha@users.noreply.github.com>
2024-01-23 23:42:08 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 5.00 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,77 +1,58 @@
 /*
-I: int n, int[][] wires - 인접행렬
-O: int
-C: n은 2 이상 100 이하인 자연수
-    wires는 길이가 n-1인 정수형 2차원 배열
-    wires의 각 원소는 [v1, v2] 2개의 자연수로 이루어져 있으며, 이는 전력망의 v1번 송전탑과 v2번 송전탑이 전선으로 연결되어 있다는 것을 의미합니다.
-    1 ≤ v1 < v2 ≤ n 
-    전력망 네트워크가 하나의 트리 형태가 아닌 경우는 입력으로 주어지지 않습니다.
-E:
-
-ds:
-algo:
-
-time:
-space:
+time: O(E * (V + E))
+space: O(V^2 + V)
 */
-import java.util.*;
 class Solution {
-    
     int[][] graph;
     boolean[] visited;
     
     public int solution(int n, int[][] wires) {
-        //edge cases
-        if (n == 2) return 0;
-        
         int answer = Integer.MAX_VALUE;
-        graph = new int[n + 1][n + 1];
         visited = new boolean[n + 1];
         
-        //인접 행렬 생성
-        for (int i = 0; i < wires.length; i++) {
-            int tower1 = wires[i][0];
-            int tower2 = wires[i][1];
-            
-            graph[tower1][tower2] = 1;
-            graph[tower2][tower1] = 1;
-        }
+        //인접행렬 만들기
+        graph = new int[n + 1][n + 1];
         
         for (int i = 0; i < wires.length; i++) {
-            //경로 하나씩 제거하기
-            int tower1 = wires[i][0];
-            int tower2 = wires[i][1];
+            int towerA = wires[i][0];
+            int towerB = wires[i][1];
             
-            graph[tower1][tower2] = 0;
-            graph[tower2][tower1] = 0;
+            graph[towerA][towerB] = 1;
+            graph[towerB][towerA] = 1;
+        }
+
+        for (int i = 0; i < wires.length; i++) {
+            int towerA = wires[i][0];
+            int towerB = wires[i][1];
             
-            Arrays.fill(visited, false);
-            int nodes1 = dfs(tower1);
+            graph[towerA][towerB] = 0;
+            graph[towerB][towerA] = 0;
             
-            Arrays.fill(visited, false);
-            int nodes2 = dfs(tower2);
+            int countA = dfs(towerA);
+            int countB = dfs(towerB);
             
-            answer = Math.min(answer, Math.abs(nodes1 - nodes2));
+            answer = Math.min(answer, Math.abs(countA - countB));
             
-            graph[tower1][tower2] = 1;
-            graph[tower2][tower1] = 1;
+            graph[towerA][towerB] = 1;
+            graph[towerB][towerA] = 1;
         }
         
         return answer;
     }
     
-    public int dfs(int startTower) {
-            
-        visited[startTower] = true;
-        int curTowers = 1;
+    public int dfs(int tower) {
+        // 현재 타워에서 갈 수 있는 타워 찾기
+        // dfs 순회하기
+        visited[tower] = true;
+        int count = 1;
         
         for (int i = 1; i < graph.length; i++) {
-            if (graph[startTower][i] == 1 && !visited[i]) {
-                curTowers += dfs(i);
+            if (!visited[i] && graph[tower][i] == 1) {
+                count += dfs(i);
             }
         }
-        visited[startTower] = false;
+        visited[tower] = false;
         
-        return curTowers;
+        return count;
     }
 }
