3e6cd41b8e9a9de2270e4ed079229ea68f19e283
Eunha Hwang <45348138+eeunha@users.noreply.github.com>
2024-01-25 15:22:16 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 2.52 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -0,0 +1,35 @@
+/*
+I: int n
+O: int[]
+C: 1 ≤ n ≤ 1,000
+E:
+
+모든 자연수 x에 대해서 현재 값이 짝수면 2로 나누기
+홀수면 3 * x + 1로 바꾸기
+무한반복 후 x가 1이 되는가?
+-> 콜라츠 문제, 콜라츠 수열
+
+콜라츠 수열 리턴
+
+*/
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        while (n >= 1) {
+            list.add(n);
+            
+            if (n == 1) break;
+            
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n = 3 * n + 1;
+            }
+        }
+        
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+}
