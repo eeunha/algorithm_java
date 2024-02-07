8e2376f6ec211f6d1bae846ec46aa0c421060716
Eunha Hwang <45348138+eeunha@users.noreply.github.com>
2024-02-07 23:49:51 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 120 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String input = br.readLine();
+        StringTokenizer st = new StringTokenizer(input, " ");
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        if (a > b)
+            bw.write(">");
+        else if (a < b)
+            bw.write("<");
+        else
+            bw.write("==");
+
+        bw.flush();
+        bw.close();
+    }
+}
