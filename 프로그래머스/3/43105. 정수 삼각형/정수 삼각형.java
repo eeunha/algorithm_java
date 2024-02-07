75f2876eac363ce9590d7762b4d734bcff2c26db
Eunha Hwang <45348138+eeunha@users.noreply.github.com>
2024-02-08 02:24:00 +0900

    [level 3] Title: 정수 삼각형, Time: 8.44 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,53 +1,41 @@
 /*
-I: int[][]
-O: int
-C: 삼각형의 높이는 1 이상 500 이하
-    삼각형을 이루고 있는 숫자는 0 이상 9,999 이하의 정수
-E: 삼각형의 높이 == 1 -> return triangle[0][0];
-
-ds: 2 dimensional array
-algo: DP
-
-[7]
-[3, 8]
-[8, 1, 0]
-[2, 7, 4, 4]
-[4, 5, 2, 6, 5]
-
-위에서부터 아래로 내려가면서 숫자를 더해간다.
-숫자가 겹치는 경우 큰 숫자를 저장한다.
-
-현재 위치를 (i, j)라고 할 때,
-triangle[i][j] += Math.max(triangle[i-1][j-1], triangle[i-1][j]);
-
-맨 마지막줄을 순회하면서 가장 큰 값을 찾아 반환하면 된다.
-
-time: O(N), N 은 triangle[][] 원소의 개수
+time: O(n), n은 triangle의 원소 수
 space: O(1)
 */
+import java.util.*;
+
 class Solution {
     public int solution(int[][] triangle) {
-        //edge cases
-        if (triangle.length == 1 && triangle[0].length == 1) return triangle[0][0];
-                                           
-        int answer = 0;
+        int length = triangle.length;
         
-        for (int i = 1; i < triangle.length; i++) {
+        //edge cases
+        if (length == 1) return triangle[0][0];
+        
+        for (int i = 1; i < length; i++) {
             for (int j = 0; j < triangle[i].length; j++) {
+                int curNumber = triangle[i][j];
+                
                 if (j == 0) {
+                    //오른쪽 값만 가져오기
                     triangle[i][j] += triangle[i - 1][j];
+                    
                 } else if (j == triangle[i].length - 1) {
+                    //왼쪽 값만 가져오기
                     triangle[i][j] += triangle[i - 1][j - 1];
+                    
                 } else {
+                    //둘 다 가져오기
                     triangle[i][j] += Math.max(triangle[i - 1][j - 1], triangle[i - 1][j]);
                 }
             }
-        }                                   
-                                           
-        for (int n : triangle[triangle.length - 1]) {
-            answer = Math.max(answer, n);
         }
-                                           
+        
+        int answer = Integer.MIN_VALUE;
+        
+        for (int i = 0; i < triangle[length - 1].length; i++) {
+            answer = Math.max(answer, triangle[length - 1][i]);
+        }
+        
         return answer;
     }
 }
