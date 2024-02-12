e00727fbe1f00524ed6035343216d3b03ae5f202
Eunha Hwang <45348138+eeunha@users.noreply.github.com>
2024-02-13 02:29:20 +0900

    [level 3] Title: N으로 표현, Time: 0.02 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -10,44 +10,46 @@ E: N == number -> return 1
 ds: map, set
 algo: dp
 
-time: O(N^2)
-space: O(N * 8)
+time: O(8 * N^2) N 은 발생할 수 있는 숫자의 개수
+space: O(8 * N)
 */
 import java.util.*;
 class Solution {
     public int solution(int N, int number) {
-        //edge case
         if (N == number) return 1;
-        
+
         Map<Integer, Set<Integer>> dp = new HashMap<>();
-        
-        for (int i = 1; i <= 8; i++) {
-            Set<Integer> set = new HashSet<>();
-            int num = 0;
-            for (int j = 0; j < i; j++) {
-                num = num * 10 + N;
-            }
-            set.add(num);
-            dp.put(i, set);
-        }
-        
+        Set<Integer> set = new HashSet<>();
+        set.add(N);
+        dp.put(1, set);
+
         for (int i = 2; i <= 8; i++) {
+            set = new HashSet<>();
+            StringBuilder sb = new StringBuilder().append(N);
             for (int j = 1; j < i; j++) {
+                sb.append(N);
+            }
+            set.add(Integer.parseInt(sb.toString())); // 기본 숫자 넣기
+            
+            for (int j = 1; j < i; j++) {
+                int k = i - j;
                 for (int num1 : dp.get(j)) {
-                    for (int num2 : dp.get(i - j)) {
-                        dp.get(i).add(num1 + num2);
-                        dp.get(i).add(num1 - num2);
-                        dp.get(i).add(num1 * num2);
+                    for (int num2 : dp.get(k)) { // 두 수의 사칙연산으로 발생하는 수 넣기
+                        set.add(num1 + num2);
+                        set.add(num1 - num2);
+                        set.add(num1 * num2);
                         if (num2 != 0) {
-                            dp.get(i).add(num1 / num2);
+                            set.add(num1 / num2);
                         }
                     }
                 }
             }
-            
-            if (dp.get(i).contains(number)) return i;
+            if (set.contains(number)) {
+                return i;
+            }
+            dp.put(i, set);
         }
-        
+
         return -1;
     }
 }
