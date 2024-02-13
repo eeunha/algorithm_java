42ca9a9e1706e9658e80c35aa99070a773217244
Eunha Hwang <45348138+eeunha@users.noreply.github.com>
2024-02-13 21:10:12 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 15.68 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -3,11 +3,7 @@ import java.util.*;
 class Solution {
     public String solution(String s) {
         String[] arr = s.split(" ");
-        int[] intArr = new int[arr.length];
-        
-        for (int i = 0; i < arr.length; i++) {
-            intArr[i] = Integer.parseInt(arr[i]);
-        }
+        int[] intArr = Arrays.stream(arr).mapToInt(Integer::parseInt).toArray();
         
         Arrays.sort(intArr);
         return intArr[0] + " " + intArr[intArr.length - 1];
