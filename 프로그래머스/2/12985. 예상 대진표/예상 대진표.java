7f47460e3ab0e284c27555501e42a0c2d48f0faf
Eunha Hwang <45348138+eeunha@users.noreply.github.com>
2024-02-17 13:44:41 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,31 @@
+/*
+8명, 4번 vs 7번
+1번째 - 3/4 7/8 -> 4/2 7/2 => 2 4
+2번째 - 1/2 3/4 -> 2/2 4/2 => 1 2
+3번째 - 1/2
+*/
+import java.util.*;
+class Solution {
+    public int solution(int n, int a, int b) {
+
+        int min = Math.min(a, b);
+        int max = Math.max(a, b);
+        
+        int answer = 1;
+
+        while (min > 0 && max > 0) {
+            //바로 붙는 경우
+            if (min % 2 == 1 && max == min + 1) {
+                return answer;    
+            }
+            
+            //다른 숫자와 붙는 경우
+            min = (min % 2 == 1) ? min / 2 + 1 : min / 2;
+            max = (max % 2 == 1) ? max / 2 + 1 : max / 2;
+                
+            answer++;
+        }
+        
+        return answer;
+    }
+}
