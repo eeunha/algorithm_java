3a2447faac9481b967cdf8f605d7138e273e98c9
Eunha Hwang <45348138+eeunha@users.noreply.github.com>
2024-02-18 16:09:50 +0900

    Update 연속 부분 수열 합의 개수.java

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -4,15 +4,18 @@ class Solution {
     public int solution(int[] elements) {
         Set<Integer> set = new HashSet<>();
         int length = elements.length;
-
+        int[] sumArr = new int[length];
+        int count = 0;
+        
         for (int i = 1; i <= length; i++) { // 자리수
             for (int j = 0; j < length; j++) { // 시작 인덱스
-                int sum = 0;
-                for (int count = 0; count < i; count++) {
-                    sum += elements[(j + count) % length];    
-                }
-                set.add(sum);
+                
+                int sum = elements[(j + count) % length];
+                sumArr[j] += sum;
+                set.add(sumArr[j]);
             }
+            
+            count++;
         }
         
         return set.size();
