0d894c3e01e9b0ca55a0d46daf9f80a683d767c8
Eunha Hwang <45348138+eeunha@users.noreply.github.com>
2024-03-01 23:41:14 +0900

    [level 0] Title: 이진수 더하기, Time: 0.05 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.java
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.java
@@ -1,20 +1,5 @@
 class Solution {
     public String solution(String bin1, String bin2) {
-        int sum = toNum(bin1) + toNum(bin2);
-        return Integer.toBinaryString(sum);
-    }
-    
-    public int toNum (String n) {
-        int num = Integer.parseInt(n);
-        int sum = 0;
-        int count = 0;
-        
-        while (num > 0) {
-            sum += Math.pow(2, count) * (num % 10);
-            num /= 10;
-            count++;
-        }
-        
-        return sum;
+         return Integer.toString(Integer.parseInt(bin1, 2) + Integer.parseInt(bin2, 2), 2);
     }
 }
