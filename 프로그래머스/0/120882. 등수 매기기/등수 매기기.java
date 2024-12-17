ceb7244bae44b88a18b7f4b1b960a694ad9fb15d
Eunha Hwang <45348138+eeunha@users.noreply.github.com>
2024-12-17 21:00:39 +0900

    [level 0] Title: 등수 매기기, Time: 0.73 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
@@ -3,32 +3,21 @@ import java.util.*;
 class Solution {
     public int[] solution(int[][] score) {
         int[] answer = new int[score.length];
-        
-        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
-        Map<Integer, Integer> map = new HashMap<>();
+        int[][] sums = new int[score.length][2];
         
         for (int i = 0; i < score.length; i++) {
-            int sum = score[i][0] + score[i][1];
-            if (map.containsKey(sum)) {
-                map.put(sum, map.get(sum) + 1);
-            } else {
-                queue.add(sum);
-                map.put(sum, 1);
-            }
+            sums[i][0] = score[i][0] + score[i][1];
+            sums[i][1] = i;
         }
         
+        Arrays.sort(sums, (a, b) -> b[0] - a[0]);
+        
         int rank = 1;
-        while (queue.size() > 0) {
-            int curSum = queue.poll();
-            int curCount = map.get(curSum);
-            
-            for (int i = 0; i < score.length && curCount > 0; i++) {
-                if (answer[i] != 0 || score[i][0] + score[i][1] != curSum) continue;
-                answer[i] = rank;
-                curCount--;
+        for (int i = 0; i < sums.length; i++) {
+            if (i > 0 && sums[i][0] != sums[i-1][0]) {
+                rank = i + 1;
             }
-            
-            rank += map.get(curSum);
+            answer[sums[i][1]] = rank;
         }
         
         return answer;
