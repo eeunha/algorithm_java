9f2513ce854f8e1172e5b10b7e26eafae843fa37
Eunha Hwang <45348138+eeunha@users.noreply.github.com>
2025-03-14 14:58:08 +0900

    [level 2] Title: 올바른 괄호, Time: 6.37 ms, Memory: 55 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,34 +1,44 @@
-import java.util.*;
-
 /*
- 23.12.26
- 
- I: String s
- O: boolean
- C: 문자열 S의 길이 <= 100000 자연수
- 	문자열 s = ( or )
- E: 시작 문자가 ) || 마지막 문자가 ( => false
- 	s.length % 2 == 1 => false
- 
- ds: 
- algo: stack
- 
- 
- time: O(n), n = s.length()
- space: O(1)
+I: String s
+O: boolean
+C: 문자열 s의 길이 : 100,000 이하의 자연수
+    문자열 s는 '(' 또는 ')' 로만 이루어져 있습니다.
+E: if s.length() % 2 != 0 > return false;
+
+ds: stack -> 여는 괄호 개수만 세기
+algo:
+
+pseudo:
+s의 길이가 홀수 -> false 리턴
+여는 괄호 개수 증가시킬 변수 생성
+s를 한 문자씩 읽기
+    여는 괄호 -> 개수 증가
+    닫는 괄호 -> 여는 괄호 개수 확인
+        여는 괄호가 있다면 수 하나 줄이기
+        없다면 false 반환
+여는 괄호 개수가 0이면 true 아니면 false 반환
+
+time: O(N) N은 s.length()
+space: O(1)
+
+solution:
 */
 class Solution {
     boolean solution(String s) {
+        // edge case
+        if (s.length() % 2 != 0) return false;
+
         int count = 0;
-
-		for (int i = 0; i < s.length(); i++) {
-
-			count += (s.charAt(i) == '(') ? 1 : -1;
-
-			if (count < 0)
-				return false;
-		}
         
-		return (count > 0) ? false : true;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                count++;
+            } else {
+                if (count == 0) return false;
+                count--;
+            }
+        }
+
+        return count > 0 ? false : true;
     }
 }
