df1f9d5ea8725db9466b426d8baa81f8540683ab
Eunha Hwang <45348138+eeunha@users.noreply.github.com>
2025-03-15 23:21:20 +0900

    [level 2] Title: 주식가격, Time: 25.31 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,27 +1,34 @@
+/*
+I: int[] prices
+O: int[]
+C: prices의 각 가격은 1 이상 10,000 이하인 자연수
+    prices의 길이는 2 이상 100,000 이하
+E:
+ 가격이 떨어지지 않은 기간은 몇초인지
+
+ds:
+algo:
+
+pseudo:
+
+time:
+space:
+
+solution:
+*/
 class Solution {
     public int[] solution(int[] prices) {
-        // edge cases
-		if (prices.length == 1)
-			return new int[] { 0 };
-
-		int[] answer = new int[prices.length];
-
-		for (int i = 0; i < prices.length - 1; i++) {
-
-			int curPrice = prices[i];
-			int time = 0;
-
-			for (int j = i + 1; j < prices.length; j++) {
-				
-				time++;
-
-				if (curPrice > prices[j])
-					break;
-			}
-
-			answer[i] = time;
-		}
-
-		return answer;
+        int[] answer = new int[prices.length];
+        
+        for (int i = 0; i < prices.length - 1; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                answer[i]++;
+                if (prices[i] > prices[j]) {
+                    break;
+                }
+            }    
+        }
+        
+        return answer;
     }
 }
