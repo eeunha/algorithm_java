ea33779584603dc941a54a42f60259bb93e40a61
Eunha Hwang <45348138+eeunha@users.noreply.github.com>
2025-03-31 01:02:07 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,41 @@
+/*
+I: String[] cards1, cards2, goal
+O: String Yes / No
+C: 1 ≤ cards1의 길이, cards2의 길이 ≤ 10
+        1 ≤ cards1[i]의 길이, cards2[i]의 길이 ≤ 10
+        cards1과 cards2에는 서로 다른 단어만 존재합니다.
+    2 ≤ goal의 길이 ≤ cards1의 길이 + cards2의 길이
+        1 ≤ goal[i]의 길이 ≤ 10
+        goal의 원소는 cards1과 cards2의 원소들로만 이루어져 있습니다.
+    cards1, cards2, goal의 문자열들은 모두 알파벳 소문자로만 이루어져 있습니다.
+E:
+
+ds:
+algo:
+
+pseudo:
+
+time: O(N+M) N은 cards1의 길이, M은 cards2의 길이
+space: O(1)
+*/
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        
+        int idx1 = 0;
+        int idx2 = 0;
+        
+        int cnt = 0;
+        
+        for (String s : goal) {
+            if (idx1 < cards1.length && s.equals(cards1[idx1])) {
+                idx1++;
+                cnt++;
+            } else if (idx2 < cards2.length && s.equals(cards2[idx2])) {
+                idx2++;
+                cnt++;
+            }
+        }
+        
+        return (cnt == goal.length) ? "Yes": "No";
+    }
+}
