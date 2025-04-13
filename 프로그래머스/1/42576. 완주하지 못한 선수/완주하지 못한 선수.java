fcdb742552964678c060035cac4333654663d446
Eunha Hwang <45348138+eeunha@users.noreply.github.com>
2025-04-13 17:48:52 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 71.12 ms, Memory: 97.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+/*
+I: String[] participant, completion
+O: String
+C: 마라톤 경기에 참여한 선수의 수는 1명 이상 100,000명 이하
+    completion의 길이는 participant의 길이보다 1 작습니다.
+    참가자의 이름은 1개 이상 20개 이하의 알파벳 소문자
+    참가자 중에는 동명이인이 있을 수 있습니다.
+E:
+
+ds: 해시셋
+algo:
+
+pseudo code:
+set 생성
+completion을 set에 넣기 -> 키는 이름, 값은 수 (동명이인)
+participant 순회
+    현재 이름이 completion에 있는지 확인
+        없다면 바로 이름 반환
+        있다면 set의 값을 1 줄이기.
+            줄인 후 값이 0이면 제거
+
+time: O(N)
+space: O(N)
+*/
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for (String c : completion) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        
+        for (String p : participant) {
+            if (map.containsKey(p)) {
+                if (map.get(p) == 1) {
+                    map.remove(p);
+                } else {
+                    map.put(p, map.get(p) - 1);
+                }
+            } else return p;   
+        }
+        
+        return "";
+    }
+}
