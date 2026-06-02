ba52b5141be377684d98cd33092629199e62a57c
Eunha Hwang <45348138+eeunha@users.noreply.github.com>
2026-06-02 18:00:25 +0900

    refactor: 의상 - 불필요한 중간 배열 제거 및 entrySet 활용 가독성 개선

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,58 +1,49 @@
 import java.util.*;
 /*
-I: String[][]
-O: int 
-C: clothes의 각 행은 [의상의 이름, 의상의 종류]
-     의상의 수는 1개 이상 30개 이하
-     같은 이름을 가진 의상은 존재하지 않습니다.
-     clothes의 모든 원소는 문자열
-     모든 문자열의 길이는 1 이상 20 이하인 자연수, 알파벳 소문자 또는 '_' 로만
-E: 
-solution:
-map을 만든다.
-의상의 종류 - 의상이름
-ex) headgear - yellow_hat, green_turban
-    eyewear - blue_sunglasses
-여기서 조합을 만든다. 고를 수 있는 모든 가지 수 구하기
-근데 문제 중복 이름 없다. -> set 써야하는가? 일단은 잘 모르겠어 보류. arraylist 쓰자.
-종류별로 최대 1개씩 고를 수 있는 조합 구하기
-
-얼굴 - 2개 => 없다, a, b => 3가지
-상의 - 1개 => 없다, a => 2가지
-하의 - 1개 => 없다, a => 2가지
-겉옷 - 1개 => 없다, a => 2가지
--> 하지만 모두 다 벗을 수는 없다.
-=> (a+1)*(b+1)*(c+1)*(d+1)-1
-
-time: O(N)
-space: O(N)
-*/
+    * 26.06.02
+    * 의상
+    *
+    * I: String[][] clothes 가지고 있는 의상
+    * O: int 서로 다른 옷의 조합의 수
+    * C: clothes의 각 행 - [의상의 이름, 의상의 종류]
+    *    1 <= 코니가 가진 의상의 수 <= 30
+    *    같은 이름의 의상은 없음
+    *    clothes의 모든 원소는 문자열
+    *    1 <= 모든 문자열의 길이 <= 20 자연수. 알파벳 소문자 또는 _
+    *    각 종류별로 최대 1가지 의상만 착용 가능
+    *    착용한 의상 일부가 겹쳐도 다른 의상이 안겹치거나 의상 추가 착용시 가능
+    *    최소 하루에 한개는 입는다
+    * E: 코니가 가진 의상의 수 == 1 > return 1
+    *
+    * ds: map
+    * algo: hash
+    *
+    * solution:
+    *   일단 map에 의상 종류 별 개수를 key, value로 넣기
+    *   그걸로 조합 구하기
+    *     = (개수 + 1) * (개수 + 1) * ... * (개수 + 1) - 1(모두 안입는 경우)
+    *
+    * time: O(N) N = clothes.length
+    * space: O(N)
+    * */
 class Solution {
     public int solution(String[][] clothes) {
+        
+		// edge case
+        if (clothes.length == 1) return 1;
+
+        int answer = 1;
+
         Map<String, Integer> map = new HashMap<>();
-		
-		for (String[] cloth: clothes) {
-			map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1);
-		}
-		
-		int[] arr = new int[map.size()];
-		
-		int idx = 0;
-		
-		for (String key : map.keySet()) {
-			
-			arr[idx] = map.get(key);
-			
-			idx++;
-		}
-		
-		int answer = 1;
 
-		for (int i = 0; i < arr.length; i++) {
-			answer *= (arr[i] + 1);
-		}
+        for (String[] cloth : clothes) {
+            map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1);
+        }
 
-		return answer - 1;
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            answer *= entry.getValue() + 1;
+        }
 
+        return answer - 1;
     }
 }
