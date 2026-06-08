5984321e7ffbbcedb43f5ae3269e9b443d51c730
Eunha Hwang <45348138+eeunha@users.noreply.github.com>
2026-06-08 15:11:18 +0900

    [level 3] Title: 베스트앨범, Time: 4.33 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,69 +1,136 @@
 /*
+26.06.08
+장르별로 가장 많이 재생된 노래 - 두개씩 모아
+노래 - 고유 번호로 구분
+노래 수록 기준
+    1. 속한 노래가 많이 재생된 장르 먼저 수록
+    2. 장르 내에서 많이 재생된 노래 먼저 수록
+    3. 장르 내에서 재생 횟수가 같은 노래  - 고유번호가 낮은 노래 먼저 수록
+
 I: String[] genres, int[] plays
-O: int[] 노래의 고유번호 배열
-C: genres[i]는 고유번호가 i인 노래의 장르
-    plays[i]는 고유번호가 i인 노래가 재생된 횟수
-    genres와 plays의 길이는 같으며, 이는 1 이상 10,000 이하
-    장르 종류는 100개 미만
-    장르에 속한 곡이 하나라면, 하나의 곡만 선택
-    모든 장르는 재생된 횟수가 다릅니다
-E:
+O: int[] 고유 번호 순서대로
+C: genres[i] - 고유번호가 i인 노래의 장르
+   plays[i] - 고유번호가 i인 노래가 재생된 횟수
+   genres.length == plays.length
+   1 <= genres.length, plays.length <= 10,000
+   genres < 100
+   장르에 속한 곡이 하나라면 하나의 곡만 선택
+   모든 장르는 재생된 횟수가 다르다
+E: genres.length == 1 && playes.length == 1 > return new int[]{0};
 
-장르별로, 가장 많이 재생된 노래 두 개씩 찾기
-노래 - 고유번호로 구분
-노래 수록 기준 
-- 1. 속한 노래가 많이 재생된 장르 수록
-- 2. 장르 내에서 많이 재생된 노래 수록
-- 3. 같은 장르의 재생 횟수가 같은 노래는 고유번호가 낮은 노래를 먼저 수록
+genres {classic, pop, classic, classic, pop}
+plays  {500,     600, 150,     800,     2500}
 
-classic - 500 + 150 + 800  = 1450
-pop - 600 + 2500 = 3100
+1차 맵 (재생수, 고유 번호(idx))
+classic - [500, 0] [150, 2] [800, 3]
+pop - [600, 1] [2500, 4]
 
-pop - classic 순서
+1. 재생 많이 된 장르부터
+classic = 500 + 150 + 800 = 1450
+pop = 600 + 2500 = 3100
+>> pop classic 순서
 
-classic - [0, 500], [2, 150], [3, 800]
-pop - [1, 600], [4, 2500]
+2. 재생 많이 된 곡부터
+pop - [2500, 4] [600, 1]
+classic = [800, 3] [500, 0] [150, 2]
 
-ds: map
-algo:
+3. 곡 수 같나? 아님.
+4. 두 장르 다 2곡 이상 존재 > 장르별 두개씩 자르기
+return [4, 1, 3, 0]
 
-time:
-space:
+ds: map, arraylist
+algo: sort
+
+solution:
+
+//edge case
+genres.length == 1 > return new int[]{0};
+
+장르별 재생 수 담을 맵 생성
+장르별 곡과 재생수를 담을 맵 생성
+
+genres 순회
+  장르별 재생수 맵 세팅
+  통합 맵 세팅
+  
+장르 맵 순회하면서 장르 이름을 넣은 리스트 만들기
+장르 재생수 기준으로 내림차순 정렬
+
+결과 리스트 생성
+
+정렬된 장르 순회
+  해당 장르 안의 곡을 재생수 기준 내림차순 정렬
+  최대 2곡 뽑아서 결과 리스트에 넣기
+
+결과 리스트를 배열로 바꾸어 반환
+
+time: O(NlogN)
+space: O(N)
 */
 import java.util.*;
-
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        ArrayList<Integer> answer = new ArrayList<>();
- 
-        HashMap<String, Integer> num = new HashMap<>(); // 장르별 총 개수
-        HashMap<String, HashMap<Integer, Integer>> music = new HashMap<>(); // 장르에 속하는 노래 및 재생횟수
-        for(int i = 0; i < plays.length; i++) {
-            if(!num.containsKey(genres[i])) {
-                HashMap<Integer, Integer> map = new HashMap<>();
-                map.put(i, plays[i]);
-                music.put(genres[i], map);
-                num.put(genres[i], plays[i]);
-            } else {
-                music.get(genres[i]).put(i, plays[i]);
-                num.put(genres[i], num.get(genres[i]) + plays[i]);
-            }
+        
+        // edge case
+        if (genres.length == 1) return new int[]{0};
+        
+        Map<String, Integer> genreMap = new HashMap<>();
+        Map<String, ArrayList<Music>> totalMap = new HashMap<>();
+        
+        for (int i = 0; i < genres.length; i++) {
+            String curGenre = genres[i];
+            int curPlay = plays[i];
+            
+            // 장르별 재생수 합산 맵 세팅
+            genreMap.put(curGenre, genreMap.getOrDefault(curGenre, 0) + curPlay);
+
+            // 장르별 곡 목록 맵 세팅
+            ArrayList<Music> curList = totalMap.getOrDefault(curGenre, new ArrayList<>());
+            curList.add(new Music(i, curPlay));
+            totalMap.put(curGenre, curList);
         }
- 
-        List<String> keySet = new ArrayList(num.keySet());
-        Collections.sort(keySet, (s1, s2) -> num.get(s2) - (num.get(s1)));
- 
-        for(String key : keySet) {
-            HashMap<Integer, Integer> map = music.get(key);
-            List<Integer> genre_key = new ArrayList(map.keySet());
- 
-            Collections.sort(genre_key, (s1, s2) -> map.get(s2) - (map.get(s1)));
- 
-            answer.add(genre_key.get(0));
-            if(genre_key.size() > 1)
-                answer.add(genre_key.get(1));
+        
+        // 장르 이름을 리스트로 만들기
+        List<String> genreList = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : genreMap.entrySet()) {
+            genreList.add(entry.getKey());
+        } 
+        
+        // 장르 이름들을 총 재생수 기준으로 정렬
+        Collections.sort(genreList, (a, b) -> genreMap.get(b) - genreMap.get(a));
+                
+        // 결과 리스트 생성
+        ArrayList<Integer> answerList = new ArrayList<>();
+        
+        // 정렬된 장르 순서대로 순회
+        for (String genre : genreList) {
+            ArrayList<Music> musics = totalMap.get(genre);
+            
+            // 해당 장르 안의 곡들을 play 기준 내림차순 정렬
+            Collections.sort(musics, (a, b) -> {
+                if (a.play == b.play) {
+                    return a.id - b.id; // 재생수 같으면 고유번호 오름차순
+                }
+                return b.play - a.play; // 다르면 재생수 내림차순
+            });
+            
+            // 최대 2개 뽑기
+            for (int i = 0; i < 2 && i < musics.size(); i++) {
+                answerList.add(musics.get(i).id);
+            }   
         }
- 
-        return answer.stream().mapToInt(i -> i).toArray();
+        
+        // answerList를 array로 바꾸어 반환
+        return answerList.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
+
+class Music {
+    int id;
+    int play;
+    
+    public Music(int id, int play) {
+        this.id = id;
+        this.play = play;
     }
 }
